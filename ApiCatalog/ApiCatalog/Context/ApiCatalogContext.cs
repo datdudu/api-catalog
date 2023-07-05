333411e90a3f5d74f967ebb208f683a5bfbb4ea1
Eduardo Carvalho Cardoso <dxcar10000@hotmail.com>
2023-07-05 19:26:22 -0300

    --COMMIT Creating classes and DbContext

---
+++ ApiCatalog/ApiCatalog/Context/ApiCatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalog.Context
+{
+    public class ApiCatalogContext : DbContext
+    {
+        public ApiCatalogContext(DbContextOptions<ApiCatalogContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
